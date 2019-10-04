82c9130960a6ff83cf6c7f1c6ad2ed13d658a9e3
Harrison McCullough <mccullough.harrison@gmail.com>
2019-10-04 10:56:30 -0600

    Reformat App.java

--- src/main/java/project/hsm/xml/dtdindent/App.java
+++ src/main/java/project/hsm/xml/dtdindent/App.java
@@ -6,28 +6,22 @@ import java.nio.charset.*;
 import java.nio.file.*;
 import org.xml.sax.*;
 
-public class App
-{
-    public static void main(String[] args)
-    {
-        try
-        {
+public class App {
+    public static void main(String[] args) {
+        try {
             String home = System.getProperty("user.home");
-            Path path = Paths
-                    .get(home, "git", "dtd-indent", "static", "example.dtd");
-            InputSource in = new InputSource(
-                    new FileInputStream(path.toFile()));
+            Path path = Paths.get(home, "git", "dtd-indent", "static",
+                                  "example.dtd");
+            InputSource in =
+                    new InputSource(new FileInputStream(path.toFile()));
             ByteArrayOutputStream out = new ByteArrayOutputStream();
             DTDParser parser = new DTDParser();
-//            parser.setDtdHandler(new DTDIndentHandler(out));
+            // parser.setDtdHandler(new DTDIndentHandler(out));
             parser.setDtdHandler(new DTDIndentHandler(System.out));
-//            parser.setDtdHandler(new DTDHandlerBase());
             parser.parse(in);
             System.out.println(
                     new String(out.toByteArray(), Charset.defaultCharset()));
-        }
-        catch (Throwable t)
-        {
+        } catch (Throwable t) {
             t.printStackTrace();
         }
     }
